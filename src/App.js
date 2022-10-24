81a6225e659db62d397a12333e4e559636ebc863
Gisele <giselemu54@gmail.com>
2022-10-24 16:41:54 +0200

    Events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+
 
 function App() {
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setlastName] = useState('');
+
+  const click1 = e=> {
+    setfirstName(e.target.value);
+
+  }
+
+    const click2 = e=> {
+      setlastName(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <form>
+      <input type="text"value={firstName} placeholder="firstName"onChange={click1}></input>
+      <input type="text"value={lastName} placeholder="lastName"onChange={click2}></input>
+      <h1>firstname:{firstName}</h1>
+      <h1>lastLName:{lastName}</h1>
+
+    </form>
+  </>
   );
-}
+
+  }
 
 export default App;
